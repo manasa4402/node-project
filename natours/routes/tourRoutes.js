8541e812db8fc503a0fec01f0d3f8a3a484cf01b
manasa koppal <manasako@amdocs.com>
2024-01-22 12:51:33 +0530

    changes with regards to mongoose and middlewares

--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -2,9 +2,17 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
-router.param('id', tourController.checkId);
+// router.param('id', tourController.checkId);
+router.route('/tour-stats').get(tourController.getTourStats);
 
-router.route('/').get(tourController.getAllTours).post(tourController.checkBody ,tourController.createTour);
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
